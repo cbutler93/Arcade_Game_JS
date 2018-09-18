2a225de6fb7d0e5cc1aecd811d116dd7ca97b708
CButler93 <chrisbutlerdev@gmail.com>
2018-09-18 13:10:20 -0600

    feat: initial object positions and player movement.
    Initiate positions of the enemy/player objects and configure basic player movement using arrow keys.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -25,12 +28,62 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+    constructor(sprite, x, y) {
+        this.sprite = sprite;
+        this.x = x;
+        this.y = y;
+    }
+
+    update(dir) {
+        switch (dir) {
+            case 'up':
+                this.y -= 83;
+                break;
+            case 'down':
+                this.y += 83;
+                break;
+            case 'left':
+                this.x -= 101;
+                break;
+            case 'right':
+                this.x += 101;
+        }
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(dir) {
+        if (dir === 'left' && this.x > 0) {
+            this.update(dir);
+        } else if (dir === 'right' && this.x < 400) {
+            this.update(dir);
+        } else if (dir === 'up' && this.y > 0) {
+            this.update(dir);
+        } else if (dir === 'down' && this.y < 373) {
+            this.update(dir);
+        }
+        this.render();
+    }
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const playerImg = 'images/char-boy.png';
+const player = new Player(playerImg, 202, 373.5);
 
-
+const allEnemies = [];
+// Calculate Y positions of enemies and add to allEnemies.
+for (let i = 0; i < 3; i++) {
+    let nextPos;
+    i === 0 ? nextPos = 41.5 : nextPos = 41.5 + (83 * i);
+    const newEnemy = new Enemy(0, nextPos, 0);
+    allEnemies.push(newEnemy);
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
